94be727e1155a7601858a8dc35e1afa04649c14d
Giovane Boaviagem Ribeiro <giovanebribeiro@gmail.com>
2021-07-20 23:08:12 -0300

    feature6: finalização seção 01

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -12,7 +12,7 @@ const MySlider = () =>{
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false,
+    arrows: true,
     adaptiveHeight: true,
     autoplay: true,
     autoplaySpeed: 7000,
@@ -29,32 +29,27 @@ const MySlider = () =>{
 
   return (
     <section id="hero">
-      <div className="row content">
-        <span><i className="quote-left fa"><FontAwesomeIcon icon={ faQuoteLeft } /></i></span>
-        <div className="text-container">
-          <div className="twelve columns">
-            <h1>{ content.title }</h1>
-          </div>
+      <div className="row">
+        
+        
           <div className="twelve columns flex-container">
           <Slider {...settings} style={{ userSelect: 'text' }}>
             { content.quotes.map((quote, index) => (
-              <div className="row offset-3">
-                <div className="pricing-tables bgrid-halves">
+              
+                
 
-                  <div className="column">
-                    <div className="price-block">
-                      <img src={ quote.img } alt={quote.alt} />
-                    </div>
-                  </div>
                   
-                </div>
-              </div>
+                    
+                      <img src={ quote.img } alt={quote.alt} />
+                    
+                  
+                  
+                
+              
             ))}
           </Slider>
           </div>
 
-        </div>
-        <span><i className="quote-right fa"><FontAwesomeIcon icon={ faQuoteRight } /></i></span>
       </div>
     </section>
   )
