ce392e76c1b286b379e79d453831b0c19a324965
Giovane Boaviagem Ribeiro <giovanebribeiro@gmail.com>
2021-08-12 01:03:17 -0300

    f15: finalização do footer e revisão geral

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
 
     /* Logo and text to show in footer */
     footer: {
-      logo: `/images/logoFooter.png`,
+      logo: `/images/AB-footer.svg`,
       text: `A escritora da esperança / mais de 1k de visualizações no Luvbook / Menção honrosa no 3º Prêmio de Literatura Juvenil Ferreira de Castro (2006 - Portugal) / Uma das vencedoras na coletânea do IX Concurso Ladjane Bandeira de Poesia `,
     },
 
